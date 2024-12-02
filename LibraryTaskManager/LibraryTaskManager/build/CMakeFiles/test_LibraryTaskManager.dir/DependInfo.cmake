2e6150b4805019dc9464d5e256090d1d05b1c2fb
AdelAlmuhtaseb <AtelAlmuhtaseb@gmail.com>
2024-12-02 21:47:30 +0200

    Initial commit

---
+++ LibraryTaskManager/LibraryTaskManager/build/CMakeFiles/test_LibraryTaskManager.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/adela/Desktop/LibraryTaskManager/book_test.cpp" "CMakeFiles/test_LibraryTaskManager.dir/book_test.cpp.obj" "gcc" "CMakeFiles/test_LibraryTaskManager.dir/book_test.cpp.obj.d"
+  "C:/Users/adela/Desktop/LibraryTaskManager/task_test.cpp" "CMakeFiles/test_LibraryTaskManager.dir/task_test.cpp.obj" "gcc" "CMakeFiles/test_LibraryTaskManager.dir/task_test.cpp.obj.d"
+  "C:/Users/adela/Desktop/LibraryTaskManager/user_test.cpp" "CMakeFiles/test_LibraryTaskManager.dir/user_test.cpp.obj" "gcc" "CMakeFiles/test_LibraryTaskManager.dir/user_test.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/adela/Desktop/LibraryTaskManager
+# Build directory: C:/Users/adela/Desktop/LibraryTaskManager/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(LibraryTests "C:/Users/adela/Desktop/LibraryTaskManager/build/test_LibraryTaskManager.exe")
+set_tests_properties(LibraryTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/adela/Desktop/LibraryTaskManager/CMakeLists.txt;44;add_test;C:/Users/adela/Desktop/LibraryTaskManager/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
