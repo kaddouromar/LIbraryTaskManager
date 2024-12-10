4152508554432a8a3e8aeaf793822976b781eab8
AdelAlmuhtaseb <AtelAlmuhtaseb@gmail.com>
2024-12-10 21:17:19 +0200

    Add files via upload

---
+++ LibraryTaskManager/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/adela/Desktop/LibraryTaskManagerV2/LibraryTaskManagerV2/LibraryTaskManagerV
+# Build directory: C:/Users/adela/Desktop/LibraryTaskManagerV2/LibraryTaskManagerV2/LibraryTaskManagerV/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(LibraryTests "C:/Users/adela/Desktop/LibraryTaskManagerV2/LibraryTaskManagerV2/LibraryTaskManagerV/build/test_LibraryTaskManager.exe")
+set_tests_properties(LibraryTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/adela/Desktop/LibraryTaskManagerV2/LibraryTaskManagerV2/LibraryTaskManagerV/CMakeLists.txt;44;add_test;C:/Users/adela/Desktop/LibraryTaskManagerV2/LibraryTaskManagerV2/LibraryTaskManagerV/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
